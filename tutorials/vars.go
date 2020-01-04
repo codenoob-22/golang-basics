86d694e23e6a8418d4b1758d1d6683d83675f4cc
Sushil Sarwade <sushil.work100@gmail.com>
2020-01-04 11:04:46 +0530

    vars basics

---
+++ tutorials/vars.go
@@ -0,0 +1,11 @@
+package main
+
+// func main() {
+// 	fmt.Println("hello this is go")
+// 	var name = "Brad"
+// 	var age = 37
+// 	const isCool = true
+// 	fmt.Println(name, age)
+// 	fmt.Printf(" %T %T\n", name, isCool)
+
+// }
