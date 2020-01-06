cd8933b72765a445d007c90ccd55de1cb6eb56b7
sushil sarwade <59190127+joblessprogrammer@users.noreply.github.com>
2020-01-06 18:43:01 +0530

    Update main.go

--- tutorials/05_arrays/main.go
+++ tutorials/05_arrays/main.go
@@ -11,4 +11,27 @@ func main() {
 
 	fmt.Println(fruit)
 	fmt.Println(fruite)
+	
+	fmt.Println("\n---------------Example 1--------------------\n")
+	for i := 0; i < len(intArray); i++ {
+		fmt.Println(intArray[i])
+	}
+
+	fmt.Println("\n---------------Example 2--------------------\n")
+	for index, element := range intArray {
+		fmt.Println(index, "=>", element)
+
+	}
+
+	fmt.Println("\n---------------Example 3--------------------\n")
+	for _, value := range intArray {
+		fmt.Println(value)
+	}
+
+	j := 0
+	fmt.Println("\n---------------Example 4--------------------\n")
+	for range intArray {
+		fmt.Println(intArray[j])
+		j++
+	}
 }
